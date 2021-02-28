cf7eed3db14b53229d776595bb0aeacf8b18ba08
Ahmed Rafik Radwan <ahmedrafik_maaar@outlook.com>
2021-02-28 13:45:20 +0200

    Add sudod to app dockerfile

--- app/bin/container_init.sh
+++ app/bin/container_init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Start logger
-service rsyslog start
+sudo service rsyslog start
 # Start cron service
-service cron start
+sudo service cron start
 # Export required env variables to a file
 echo export CONFIG_FILE=$CONFIG_FILE > $HOME/.cron_profile
 # Run apache in the foreground
-apache2-foreground
+sudo apache2-foreground
